06cecc999e2bf0bb0dd1d26b936374c6eb5eb4d7
Chris Black <christopher_black@brown.edu>
2025-11-08 11:23:59 +0000

    updated ClimbingSessionData class

--- climbing_analysis/data/session_data.py
+++ climbing_analysis/data/session_data.py
@@ -2,10 +2,12 @@ from pathlib import Path
 from climbing_analysis.decorators import log_call
 from climbing_analysis.pose.utils import load_df_list, load_pickle
 from climbing_analysis.ephys.spike_sorting import *
+from climbing_analysis.ephys.events import get_camera_events
 
 class ClimbingSessionData:
     def __init__(self, session_path):
         self.session_path = Path(session_path)
+        self.pose_path = self.session_path / 'PoseData'
         self.ephys_data = None
         self.pose_data = None
         self.metadata = {}
@@ -18,23 +20,21 @@ class ClimbingSessionData:
 
         self.check_pose()
         self.check_ephys()
+        self.get_event_data()
 
     @log_call(label='pose data check')
     def check_pose(self):
-        for child in self.session_path.iterdir():
-            if child.name == 'PoseData':
-                child_ = Path(child)
-                if child_.is_dir():
-                    self.pose_path = child_
-                    self.get_pose_data(self.pose_path)
-                else:
-                    print('no pose data available.')
+        if self.session_path.is_dir():
+            self.get_pose_data(self.pose_path)
+        else:
+            print('no pose data available.')
 
     @log_call(label='ephys data check')
     def check_ephys(self):
         self.sorting_path = self.session_path.joinpath(self.rec_node, self.experiment_no, self.recording_no, self.sorter_method, 'phy_output')
         if self.sorting_path.exists():
-            self.get_ephys_data()
+            self.get_spike_data()
+            #self.get_lfp_data()
 
     @log_call(label='pose data', type='load')
     def get_pose_data(self, pose_path):
@@ -42,6 +42,25 @@ class ClimbingSessionData:
         self.pose_df_list = load_df_list(pose_path / 'pose.h5')
         self.stances = load_pickle(pose_path / 'stances.pkl')
 
-    @log_call(label='ephys data', type='load')
-    def get_ephys_data(self):
+    @log_call(label='spike data', type='load')
+    def get_spike_data(self):
         self.sorter = load_phy_sorting(self.sorting_path)
+
+    @log_call(label='event data', type='load')
+    def get_event_data(self):
+        event_data, ts, bouts, frame_captures, continuous = get_camera_events(self.session_path)
+        self.frame_captures = frame_captures
+
+    @property
+    def unit_ids(self):
+        if self.sorter:
+            return self.sorter.get_unit_ids()
+        else:
+            raise AttributeError('sorting object does not exist, please run spike sorting.')
+    @property
+    def trial_number(self):
+        if self.pose_df_list:
+            return len(self.pose_df_list)
+        else:
+            raise AttributeError('pose data does not exist, please add to root folder.')
+        
