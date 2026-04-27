a27ba4e424d8cf7be9f8cfd7ccdabed780f8778e
Chris Black <christopher_black@brown.edu>
2026-04-27 17:52:17 +0100

    Updated docstrings for processed class functions

--- neurokinematics/data/processed.py
+++ neurokinematics/data/processed.py
@@ -14,7 +14,7 @@ from neurokinematics.io import load_zarr, load_memmap, load_json, load_csv, load
 
 @dataclass
 class LFPProcessed:
-    """Lightweight class for preprocessed lfp data.
+    """Lightweight class for preprocessed lfp data and associated metadata.
     """
     output_path: Path
     shape: tuple
@@ -31,7 +31,7 @@ class LFPProcessed:
             return_metadata (bool, optional): _description_. Defaults to False.
 
         Returns:
-            _type_: _description_
+            data (array) or data, metadata (dict): Lazy load data with zarr or np.memmap depending on storage_format. Optionally return dictionary of metadata.
         """
         if self.storage_format == "memmap":
             data = load_memmap(self.output_path, shape=self.shape, dtype=self.dtype, mode='r')
@@ -54,10 +54,27 @@ class PoseProcessed:
     movement_output_path: Optional[Path] = None
 
     def load_pose(self, pkg_format: str = 'dask'):
+        """Load pose estimation data into dataframe.
+
+        Args:
+            pkg_format (str, optional): Method to use for loading data. Options are 'dask', 'pandas'. Using 'dask' provides a lazy load, while 'pandas' will load into memory. Defaults to 'dask'.
+
+        Returns:
+            data (dataframe): Dataframe containing preprocessed pose estimation data.
+        """
         if self.storage_format == "csv":
             data = load_csv(self.pose_output_path, pkg_format=pkg_format)
             return data
+        
     def load_movement(self, pkg_format: str = 'pandas'):
+        """Load movement event data into dataframe using neurokinematics.io.load_pickle
+
+        Args:
+            pkg_format (str, optional): Method to use when reading in data. Options are 'default', which uses pickle, or 'pandas'. Defaults to 'pandas'.
+
+        Returns:
+            data (dataframe): Dataframe containing extracted movement events.
+        """
         if type(self.movement_output_path) == pathlib.WindowsPath:
             data = load_pickle(self.movement_output_path, pkg_format)
             return data
