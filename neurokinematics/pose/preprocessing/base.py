e5b88b48d7395701bce2936f0fa07bf8dc5b3e2c
Chris Black <christopher_black@brown.edu>
2026-04-27 17:37:41 +0100

    Updated docstring and comments

--- neurokinematics/pose/preprocessing/base.py
+++ neurokinematics/pose/preprocessing/base.py
@@ -1,7 +1,12 @@
 """ 
 Batch preprocessing markerless pose estimation data.
 
+This module provides utilties for loading, preprocessing, and saving pose data from markerless pose-estimation pipelines.
+It is currently only tested with SLEAP data converted to the '.h5' format. DLC and other pose estimation models, and file formats are will be developed in the future.
 
+Main function, `process_sleap`, loads pose files from a single directory, applies pre-defined preprocessing of pose data, and optionally extracts movement events. 
+
+Details of pre-processing steps and movement event extraction are set by the selected config file from `neurokinematics/configs/pose_cfg`. These files can be used as templates for custom configs.
 """
 
 from pathlib import Path
@@ -19,7 +24,16 @@ from neurokinematics.pose.movement_events import extract_movements
 
 
 def process_sleap(data_path: str, pose_cfg: str):
-    
+    """Preprocess markerless pose data from sleap stored in a single directory.
+
+    Args:
+        data_path (str): Path to folder containing sleap `.h5` files
+        pose_cfg (str): Config file stored in `configs/pose_cfg` directory
+
+    Returns:
+        PoseProcessed: Lightweight class storing metadata for preprocessing steps
+    """
+
     # now requires config file
     cfg = load_config(pose_cfg, config_type='pose') #POSE_PREPROCESSING_CONFIG_PATH / preprocess_cfg)
     preprocessing = cfg['pose_preprocessing']
@@ -27,7 +41,7 @@ def process_sleap(data_path: str, pose_cfg: str):
     sample_rate = cfg['pose_format']['frame_rate']
     movement_detection = cfg['movement_detection']
 
-    # get / creat paths
+    # get / create paths
     data_path = Path(data_path)
     pose_path = data_path / 'pose'
     pose_path.mkdir(exist_ok=True)
