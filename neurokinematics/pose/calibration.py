72dbbfe5e161c1f7d8711cd0955d5b6494955f0e
Chris Black <christopher_black@brown.edu>
2026-05-01 00:34:51 +0100

    Added docustring and comments for pixels_to_cm function

--- neurokinematics/pose/calibration.py
+++ neurokinematics/pose/calibration.py
@@ -1,5 +1,21 @@
+""" 
+This module contains helper functions for experimental calibrations.
+
+This module will require future updates for other behaviours.
+
+"""
+
 
 def pixels_to_cm(wall='2mm_basic'):
+    """Simple call to get pixel conversion to centimeters for pose estimation data. This function will be updated in the future for other behaviour experiments.
+
+    Args:
+        wall (str, optional): Experimental mode. Defaults to '2mm_basic'.
+
+    Returns:
+        float: Conversion between pixels and centimeters
+    """
+
     # wall distance 6mm average = 25.3px
     # wall distance 6mm std = 1.6px
     if wall == '2mm_basic':
