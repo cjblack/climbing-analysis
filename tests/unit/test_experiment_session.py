12a9ae1db6dce0309c6cebc105e10803164174bf
Chris Black <christopher_black@brown.edu>
2026-04-30 13:09:18 +0100

    New test for ExperimentSession

---
+++ tests/unit/test_experiment_session.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+import yaml
+
+from neurokinematics.data.session import ExperimentSession
+
+def test_experiment_session_create_and_reload(tmp_path):
+    # create dummy dirs
+    ephys_path = tmp_path / 'ephys_data_path'
+    pose_path = tmp_path / 'pose_data_path'
+    output_path = tmp_path / 'output_root_path'
+
+    ephys_path.mkdir()
+    pose_path.mkdir()
+    output_path.mkdir()
+
+    # create session
+
+    session = ExperimentSession(
+        session_id = "pytest_session",
+        ephys_data_path = ephys_path,
+        pose_data_path = pose_path,
+        output_root_path = output_path,
+        cfg = 'demo_session.yaml'
+    )
+
+    # base checks
+    assert session.session_path.exists() # check session path created
+    assert (session.session_path / "session_config.yaml").exists() # check session config created during instantiation
+
+    # reload session
+    reloaded = ExperimentSession.from_existing(session.session_path)
+
+    # consistency
+    assert reloaded.session_id == session.session_id
+    assert reloaded.ephys_data_path == session.ephys_data_path
+    assert reloaded.pose_data_path == session.pose_data_path
+    assert reloaded.session_path == session.session_path
+
+def test_invalid_paths(tmp_path):
+    with pytest.raises(FileNotFoundError):
+        ExperimentSession(
+            session_id = "pytest_session_invalid",
+            ephys_data_path = tmp_path / 'ephys_data_path',
+            pose_data_path = tmp_dir / 'pose_data_path',
+            output_root_path = tmp_path,
+            cfg = 'demo_session.yaml'
+        )
