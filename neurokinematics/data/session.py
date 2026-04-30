36d30a0b53c6ac66c3965c9e5337cdbd0c273666
Chris Black <christopher_black@brown.edu>
2026-05-01 00:13:25 +0100

    Updated docstrings and added validation checks for epoching LFP and spikes

--- neurokinematics/data/session.py
+++ neurokinematics/data/session.py
@@ -27,13 +27,16 @@ from neurokinematics.multi_modal.alignment import get_camera_events, align_movem
 
 
 class ExperimentSession:
-    """
-    Class for loading in all relevant pose and ephys data during climbing session.
-    Requires processing of video data for pose estimation to be stored within data directory in 'PoseData' folder.
-    
-    Use:
-    from neurokinematics.data.session import ExperimentalSessionData
-    csession = ClimbingSessionData('path/to/data/directory')
+    """Class for orchestrating preprocessing and alignment of ephys and pose data
+
+    Example:
+        >>> session = ExperimentSession(
+        ...     session_id = "demo_session",
+        ...     ephys_data_path = "path/containing/ephys/data",
+        ...     pose_data_path = "path/containing/pose/data",
+        ...     output_root_path = "path/to/store/neurokinematics/session"
+        ... )
+        >>> session.preprocess_and_align()
     """
     def __init__(self, session_id: str, ephys_data_path: Path | str, pose_data_path: Path | str, output_root_path: Path | str | None = None, cfg: str ='demo_session.yaml'):
   
@@ -78,15 +81,26 @@ class ExperimentSession:
 
     @classmethod
     def from_existing(cls, session_path: Path | str):
+        """Loads previously created neurokinematics session
+
+        Args:
+            session_path (Path | str): Path to previously created session 
+
+        Returns:
+            ExperimentSession: Object replicating the original instatiated session
+        """
         session_path = Path(session_path)
 
         session_config_path = session_path / "session_config.yaml"
 
+        # load previously created config file
         with open(session_config_path, "r") as f:
             cfg = yaml.safe_load(f)
         
+        # get runtime config information
         runtime = cfg['session_runtime']
 
+        # setup session
         session = cls(
             session_id = runtime['session_id'],
             ephys_data_path = runtime['ephys_data_path'],
@@ -102,6 +116,7 @@ class ExperimentSession:
         session.multimodal_cfg = cfg['configs']['multimodal']
         session.sorting_cfg = cfg['configs']['spikes']
 
+        # get metadata
         session.metadata = cfg.get('session_metadata', {})
         if not session.metadata:
             session._set_metadata()
@@ -173,12 +188,16 @@ class ExperimentSession:
             yaml.safe_dump(cfg, f, sort_keys=False)
     
     def _set_metadata(self):
+        """Sets metadata for session - will be expanded in future updates
+        """
+
         self.metadata = {
             'ephys': {
                 'acquisition': self.cfg['session']['ephys']['acquisition'],
                 'sample_rate': self.sorting_cfg['sample_rate'],
                 'lfp_node_idx': self.cfg['session']['ephys']['lfp']['node_idx'],
                 'lfp_rec_idx': self.cfg['session']['ephys']['lfp']['rec_idx'],
+                'spike_sorter': self.sorting_cfg['sorter']
             },
             'pose':{
                 'frame_rate': self.pose_cfg['pose_format']['frame_rate'],
@@ -188,6 +207,19 @@ class ExperimentSession:
         }
     
     def _handle_existing_output(self, path: Path, mode: str):
+        """Deals with processing/alignment calls if session was already created to avoid accidental overwriting
+
+        Args:
+            path (Path): Path to expected data location
+            mode (str): Mode to execute, options are 'skip' to skip over function call, 'overwrite' to perform function call, and 'error' to check for accidental overlap
+
+        Raises:
+            ValueError: Error if the incorrect mode was selected
+            FileExistsError: Error check if the expected output already exists
+
+        Returns:
+            bool: Boolean determines whether downstream function call is executed
+        """
 
         if mode not in {"skip", "overwrite", "error"}:
             raise ValueError("Mode must be one of: 'skip', 'overwrite', 'error'")
@@ -208,18 +240,25 @@ class ExperimentSession:
                 path.unlink()
             return True
 
-    
     def preprocess_and_align(self):
-
-        self.run_pose_processing()
-        self.run_spike_sorting()
-        self.run_lfp_processing()
-        self.align_video()
-        self.align_movements()
+        """Performs all preprocessing and alignment steps
+        """
+        self.run_pose_processing() # process pose data
+        self.run_spike_sorting() # run spike sorting
+        self.run_lfp_processing() # process LFP data
+        self.align_video() # align video to ephys
+        self.align_movements() # align movement events to ephys
 
     @log_call(label='pose preprocessing', type='run')
     def run_pose_processing(self, mode: str = "skip"):
+        """Run preprocessing on markerless pose data and store results
 
+        Args:
+            mode (str, optional): Determines whether to perform processing step. Options are 'skip, 'overwrite', or 'error'. 'skip' will be able to run if data isn't present, so reloading session will skip automatically. Use 'overwrite' to re-run. Defaults to "skip".
+
+        Returns:
+            Path: If processing occured and mode is 'skip', returns path to processed data
+        """
         # overwrite utility
         expected_output = self.dirs['pose'] / 'pose_data.csv'
 
@@ -236,6 +275,15 @@ class ExperimentSession:
     
     @log_call(label='spike sorting', type='run')
     def run_spike_sorting(self, mode: str = "skip"):
+        """Run spike sorting through SpikeInterface and store results
+
+        Args:
+            mode (str, optional): Determines whether to perform processing step. Options are 'skip, 'overwrite', or 'error'. 'skip' will be able to run if data isn't present, so reloading session will skip automatically. Use 'overwrite' to re-run. Defaults to "skip".
+
+        Returns:
+            Path: If spike sorting occured and mode is 'skip', returns path to sorting results
+        """
+
         
         # overwrite utility
         expected_output = self.dirs['spikes'] / 'sorting_analyzer'
@@ -253,9 +301,17 @@ class ExperimentSession:
     
     @log_call(label='lfp preprocessing', type='run')
     def run_lfp_processing(self, mode: str = "skip"):
+        """Run preprocessing on raw LFP data (chunk, filter, downsample) and store results
+
+        Args:
+            mode (str, optional): Determines whether to perform processing step. Options are 'skip, 'overwrite', or 'error'. 'skip' will be able to run if data isn't present, so reloading session will skip automatically. Use 'overwrite' to re-run. Defaults to "skip".
+
+        Returns:
+            Path: If processing occured and mode is 'skip', returns path to processed data
+        """
 
         # overwrite utility
-        expected_output = self.dirs['lfp'] / 'lfp_processed'
+        expected_output = self.dirs['lfp'] / 'lfp_preprocessed'
 
         should_run = self._handle_existing_output(expected_output, mode)
 
@@ -316,12 +372,14 @@ class ExperimentSession:
             Path: If alignment exists and mode is 'skip', returns path to alignment
         """
 
+        # set required files for running alignment that should have been created in the session
         required = [
             self.dirs['pose'] / 'movement_events.pkl',
             self.dirs['pose'] / 'pose_data.csv',
             self.dirs['alignment'] / 'video_alignment.csv'
         ]
 
+        # indicate missing files
         missing = [p for p in required if not p.exists()]
         if missing:
             raise FileNotFoundError(
@@ -329,19 +387,20 @@ class ExperimentSession:
                 + "\n".join(str(p) for p in missing)
             )
         
+        # check whether to skip or overwrite alignment
         expected_output = self.dirs['alignment'] / 'movement_event_alignment.csv'
-
         should_run = self._handle_existing_output(expected_output, mode)
-
         if not should_run:
             return expected_output
-
+        
+        # set alignment dirs - 'events' dir is redundant, remove in future updates
         dirs_for_alignment = {
             'events': self.dirs['pose'],
             'pose': self.dirs['pose'],
             'alignment': self.dirs['alignment']
         }
 
+        # run alignment
         self.aligned_movements = align_movements_to_ephys(
             dirs = dirs_for_alignment,
             fs = self.ephys_sample_rate, #self.sorting_cfg['sample_rate'],
@@ -355,7 +414,27 @@ class ExperimentSession:
         Args:
             mode (str, optional): Determines whether to epoch data. Options are 'skip, 'overwrite', or 'error'. 'skip' will be able to run if data isn't present, so reloading session will skip automatically. Use 'overwrite' to re-run. Defaults to "skip".
         """
-        # need to add validation logic
+
+        # set required files for running alignment that should have been created in the session
+        required = [
+            self.dirs['alignment'] / 'movement_event_alignment.csv',
+            self.dirs['lfp'] / 'lfp_preprocessed'
+        ]
+
+        # indicate missing files
+        missing = [p for p in required if not p.exists()]
+        if missing:
+            raise FileNotFoundError(
+                "Cannot epoch LFP. Missing required files:\n"
+                + "\n".join(str(p) for p in missing)
+            )
+
+        # check whether to skip or overwrite epoching
+        expected_output = self.dirs['lfp'] / 'lfp_epoched'
+        should_run = self._handle_existing_output(expected_output, mode)
+        if not should_run:
+            return expected_output
+        
         self.epoch_lfp_root = get_movement_aligned_erps(
             alignment = self.dirs['alignment'] / 'movement_event_alignment.csv',
             lfp_data = self.dirs['lfp'] / 'lfp_preprocessed',
@@ -368,8 +447,28 @@ class ExperimentSession:
         Args:
             mode (str, optional): Determines whether to epoch data. Options are 'skip, 'overwrite', or 'error'. 'skip' will be able to run if data isn't present, so reloading session will skip automatically. Use 'overwrite' to re-run. Defaults to "skip".
         """
-        # need to add validation logic
-        self.sorter = load_phy_sorting(self.dir['spikes'])
+
+        # set required files for running alignment that should have been created in the session
+        required = [
+            self.dirs['alignment'] / 'movement_event_alignment.csv',
+            self.dir['spikes'] / self.spike_sorting_method
+        ]
+
+        # indicate missing files
+        missing = [p for p in required if not p.exists()]
+        if missing:
+            raise FileNotFoundError(
+                "Cannot epoch LFP. Missing required files:\n"
+                + "\n".join(str(p) for p in missing)
+            )
+
+        # check whether to skip or overwrite epoching
+        expected_output = self.dirs['spikes'] / 'rasters' / 'movement_aligned_rasters.pkl'
+        should_run = self._handle_existing_output(expected_output, mode)
+        if not should_run:
+            return expected_output
+        
+        self.sorter = load_phy_sorting(self.dir['spikes'] / self.spike_sorting_method)
         self.spike_raster_obj = get_movement_aligned_rasters(
             alignment = self.dirs['alignment'] / 'movement_event_alignment.csv',
             sorter = self.sorter,
@@ -378,19 +477,33 @@ class ExperimentSession:
     
     @property
     def ephys_sample_rate(self):
+        """ Returns sample rate of ephys acquisition
+        """
         return self.metadata['ephys']['sample_rate']
     
     @property
     def pose_sample_rate(self):
+        """ Returns frame rate of original pose data
+        """
         return self.metadata['pose']['frame_rate']
     
     @property
     def acquisition_system(self):
+        """ Returns name of ephys acquisition system
+        """
         return self.metadata['ephys']['acquisition']
     
     @property
-    def pose_type(self):
+    def pose_package(self):
+        """ Returns name of package used for pose data 
+        """
         return self.metadata['pose']['pose_type']
+    
+    @property
+    def spike_sorter(self):
+        """ Returns name of spike sorter used in session
+        """
+        return self.metadata['spikes']['spike_sorter']
 
 # class ClimbingSession:
 #     """
