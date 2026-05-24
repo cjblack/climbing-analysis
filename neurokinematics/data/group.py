18a6ce1942a5252b748cbff212d5bc033af6b030
Chris Black <christopher_black@brown.edu>
2026-05-24 11:25:39 +0100

    WIP: added group module to orchestrate multiple subjects

---
+++ neurokinematics/data/group.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+from neurokinematics.data import ExperimentSession, ExperimentSubject
+
+class ExperimentGroup:
+
+    def __init__(self):
+        
+        pass 
+    def add_subjects(self):
+        pass
